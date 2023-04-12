109fddd971e7bfee00a201c938a3f67d035817d3
unknown <jackfortin0220@gmail>
2023-04-12 13:28:33 -0400

    deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,157 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { TreeView } from "@mui/lab";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import TreeItem from "@mui/lab/TreeItem";
+import { useState } from "react";
+import { Alert } from "@mui/material";
 
-const inter = Inter({ subsets: ['latin'] })
+interface RenderTree {
+  id: string;
+  name: string;
+  children?: readonly RenderTree[];
+}
+
+const data: RenderTree = {
+  id: "1",
+  name: "P: NONE ID: 1",
+  children: [
+    {
+      id: "2",
+      name: "P: 1 ID: 2",
+    },
+    {
+      id: "3",
+      name: "P: 1 ID: 3",
+      children: [
+        {
+          id: "4",
+          name: "P: 3 ID: 4",
+        },
+      ],
+    },
+  ],
+};
+
+const testData: RenderTree = {
+  id: "1",
+  name: "P: NONE ID: 1",
+  children: [
+    {
+      id: "2",
+      name: "P: 1 ID: 2",
+    },
+    {
+      id: "3",
+      name: "P: 1 ID: 3",
+      children: [
+        {
+          id: "4",
+          name: "P: 3 ID: 4",
+        },
+      ],
+    },
+  ],
+};
+
+const findNodeById: any = (obj: RenderTree, id: string) => {
+  if (obj.id === id) return obj;
+  if (obj.children) {
+    for (let i = 0; i < obj.children.length; i++) {
+      const foundNode = findNodeById(obj.children[i], id);
+      if (foundNode) return foundNode;
+    }
+  }
+  return null;
+};
+
+const countNodes = (node: RenderTree) => {
+  let count = 1;
+
+  if (node.children) {
+    for (let child of node.children) {
+      count += countNodes(child);
+    }
+  }
+  return count;
+};
 
 export default function Home() {
+  const [treeData, setTreeData] = useState(data);
+  const [PID, setPID] = useState("");
+  const [isExist, setIsExist] = useState(true);
+
+  const handleChange = (event: any) => {
+    console.log(event.target.value);
+    setPID(event.target.value);
+  };
+
+  const handleClick = () => {
+    setIsExist(true);
+    let data = treeData;
+    const new_data = addChild(data, `${PID}`);
+    setTreeData(new_data);
+    setPID("");
+  };
+
+  const addChild = (obj: RenderTree, pid: string) => {
+    const parent = findNodeById(obj, pid);
+
+    if (parent) {
+      const randomId = `${countNodes(data) + 1}`;
+      if (parent.children) {
+        parent.children.push({
+          id: randomId,
+          name: `P: ${parent.id} ID: ${randomId}`,
+        });
+      } else {
+        parent.children = [
+          {
+            id: randomId,
+            name: `P: ${parent.id} ID: ${randomId}`,
+          },
+        ];
+      }
+    } else {
+      setIsExist(false);
+    }
+    return obj;
+  };
+
+  const renderTree = (nodes: RenderTree) => (
+    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
+    </TreeItem>
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div className="mt-4 ml-4">
+      <TreeView
+        aria-label="rich object"
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpanded={["1"]}
+        defaultExpandIcon={<ChevronRightIcon />}
+        sx={{ height: 410, flexGrow: 1, maxWidth: 400, overflowY: "auto" }}
+        className="rounded border mb-4"
+      >
+        {renderTree(treeData)}
+      </TreeView>
+      <label>Add Node To:</label>
+      <input
+        type="number"
+        className="ml-4 mr-auto p-1 shadow text-xs"
+        placeholder="PID"
+        value={PID}
+        onChange={handleChange}
+      />
+      <br />
+      <Alert severity="error" className="w-[270px] mt-2" hidden={isExist}>
+        This PID is not exist!
+      </Alert>
+      <button className="mt-2 px-2 py-1 mr-2 button" onClick={handleClick}>
+        Add
+      </button>
+    </div>
+  );
 }
